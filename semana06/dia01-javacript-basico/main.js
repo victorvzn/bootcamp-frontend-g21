28e935bbf0f758c81b8577ee11cdd34098583c82
Victor Villazón <victorzn23@gmail.com>
2024-09-11 20:33:04 -0500

    semana06, dia01, javascript basico

--- semana06/dia01-javacript-basico/main.js
+++ semana06/dia01-javacript-basico/main.js
@@ -11,7 +11,7 @@
   líneas
 */
 
-// TIPOS DE DATOS (...)
+// TIPOS DE DATOS (https://lenguajejs.com/javascript/)
 
 // PRIMITIVOS: Number, String, Boolean (true, false), undefined, null, BigInt, Symbol
 
@@ -56,3 +56,39 @@ console.log(typeof apellido)
 
 // BigInt (Permite almacenar numeros muy grandes)
 // Symbol (Ayudan a crear valores únicos e irrepetibles)
+
+// VARIABLES Y CONSTANTES
+
+// ECMASCRIPT 5 === ES5
+// ECMASCRIPT 6 === ES6
+
+// var -> ES5 (Forma de declarar una variable pero no es recomendado su uso, la declaración es a nivel de la función) ❌
+
+// CONST -> ES6 (El uso de const es lo recomendado) ✅
+
+const edad = 55 // Es obligatorio inicializarlo
+const _edad = 25
+const edadDeMiHijoMayor = 15
+const PI = 3.14
+const empresa = 'JS'
+const esMayorDeEdad = false
+
+// edad = 90 // Uncaught TypeError: Assignment to constant variable.
+
+console.log(edad)
+
+// LET -> ES6 (El uso de let e lo recomendado) ✅
+
+let edad2 = 35 // No es obligatorio inicializarlo
+
+edad2 = 39 // estamos reasignando un valor numérico
+console.log(typeof edad2)
+edad2 = "victor"
+console.log(typeof edad2)
+edad2 = true
+console.log(typeof edad2)
+
+console.log('1121' + 15  + 19)
+
+// OPERADORES MATEMÁTICOS
+
