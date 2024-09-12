c12148ca5626e2b841f9f813968cd8632cceb2c3
Victor Villazón <victorzn23@gmail.com>
2024-09-11 20:06:03 -0500

    semana06, dia01, javascript basico

--- semana06/dia01-javacript-basico/main.js
+++ semana06/dia01-javacript-basico/main.js
@@ -1,2 +1,58 @@
+// COMENTARIOS
+
 // console.log('Hola a todos!')
 
+/*
+  Esto
+  es 
+  un
+  comentario
+  de múltiples
+  líneas
+*/
+
+// TIPOS DE DATOS (...)
+
+// PRIMITIVOS: Number, String, Boolean (true, false), undefined, null, BigInt, Symbol
+
+// NUMBER
+
+console.log(20);
+console.log(15.69)
+console.log(0.9999)
+console.log(-36)
+
+console.log(typeof 20); // number
+console.log(typeof 15.69) // number
+console.log(typeof 0.9999) // number
+console.log(typeof -36) // number
+
+// STRING
+
+console.log('Claudia')
+console.log("Farit")
+console.log("200")
+
+console.log(typeof 'Claudia') // string
+console.log(typeof "Farit") // string
+console.log(typeof "200") // string
+
+// BOOLEAN -> false o true
+
+console.log(true)
+console.log(typeof false)
+
+// UNDEFINED
+
+let nombre // Declaración de una variable
+console.log(nombre)
+console.log(typeof nombre)
+
+// NULL
+
+let apellido = null
+console.log(apellido)
+console.log(typeof apellido)
+
+// BigInt (Permite almacenar numeros muy grandes)
+// Symbol (Ayudan a crear valores únicos e irrepetibles)
