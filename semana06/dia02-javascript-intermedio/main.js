8c5f3fbdfa43a52f801f3732b3d3be2aec307f4d
Victor Villazón <victorzn23@gmail.com>
2024-09-12 19:42:14 -0500

    semana06, dia02, javascript intermedio

--- semana06/dia02-javascript-intermedio/main.js
+++ semana06/dia02-javascript-intermedio/main.js
@@ -1,1 +1,20 @@
-console.log('Hola JS')
+// console.log('Hola JS')
+
+// TIPOS DE DATOS NO PRIMITIVOS
+
+// ARRAYS
+
+// Un arreglo tiene elementos de cualquier tipo de dato: cadena, números, boolean, null, arrays, objetos, etc.
+
+// DECLARARACIÓN
+
+const arregloVacio = []
+const listaDeNombres = ['Victor', 'Alessandro', 'Francesca', 'Yesica']
+const listaDeNumeros = [25, 36, 89, 1560]
+const listaDeValores = [1, 2 , 3, 'Victor', 'Villazón', true, null, undefined]
+
+console.log(arregloVacio)
+console.log(listaDeNombres)
+console.log(listaDeNumeros);
+console.log(listaDeValores)
+
