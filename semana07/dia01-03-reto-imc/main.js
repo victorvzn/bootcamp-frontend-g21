6e9af3b4a9490f865ae545ebde486234438e7d50
Victor Villazón <victorzn23@gmail.com>
2024-09-18 23:07:41 -0500

    semana07, dia01, DOM - key detect

--- semana07/dia01-03-reto-imc/main.js
+++ semana07/dia01-03-reto-imc/main.js
@@ -1,1 +1,11 @@
-console.log('Hola JS')
+// console.log('Hola JS')
+
+// const buttonCalcular = document.querySelector('#calcular')
+const inputPeso = document.getElementById('peso')
+const inputAltura = document.getElementById('altura')
+const buttonCalcular = document.getElementById('calcular')
+const sectionResultado = document.getElementById('resultado')
+
+buttonCalcular.addEventListener('click', function(event) {
+  console.log('click')
+})
