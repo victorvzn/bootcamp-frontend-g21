2f68cd6d5dff1af345e7a875ebcd0b6c4a957d92
Victor Villazón <victorzn23@gmail.com>
2024-09-18 21:02:22 -0500

    semana07, dia01, DOM - key detect

--- semana07/dia01-02-calculadora/main.js
+++ semana07/dia01-02-calculadora/main.js
@@ -3,7 +3,18 @@
 const inputDisplay = document.querySelector('.display')
 const buttons = document.querySelectorAll('button')
 
+buttons.forEach(function(button) {
+  // console.log(button.textContent)
 
+  button.addEventListener('click', function(event) {
+    console.log({ target: event.target })
+    const buttonText = event.target.textContent
+
+    console.log(buttonText) // + 9 8 1 =
+
+    // TODO: Añadiremos la lógica de la calculadora
+  })
+})
 
 // console.log(buttons)
 // console.log({inputDisplay})
