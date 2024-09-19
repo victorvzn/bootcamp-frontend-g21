9d100787a752352064ff785a5541c658ec910b37
Victor Villazón <victorzn23@gmail.com>
2024-09-18 20:44:23 -0500

    semana07, dia01, DOM - key detect

---
+++ semana07/dia01-02-calculadora/main.js
@@ -0,0 +1,10 @@
+// console.log('Hola JS')
+
+const inputDisplay = document.querySelector('.display')
+const buttons = document.querySelectorAll('button')
+
+
+
+// console.log(buttons)
+// console.log({inputDisplay})
+// inputDisplay.value = '9999'
