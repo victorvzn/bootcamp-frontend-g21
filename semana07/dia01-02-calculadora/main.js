e02195a34a256866f711822f632e141fdcc1ee58
Victor Villazón <victorzn23@gmail.com>
2024-09-18 21:35:57 -0500

    semana07, dia01, DOM - key detect

--- semana07/dia01-02-calculadora/main.js
+++ semana07/dia01-02-calculadora/main.js
@@ -1,5 +1,9 @@
 // console.log('Hola JS')
 
+let numeroActual = '0'
+let operador = '' // + * -
+let operando = '' // Cualquier número. Ej. 8, 968
+
 const inputDisplay = document.querySelector('.display')
 const buttons = document.querySelectorAll('button')
 
@@ -7,12 +11,26 @@ buttons.forEach(function(button) {
   // console.log(button.textContent)
 
   button.addEventListener('click', function(event) {
-    console.log({ target: event.target })
+    // console.log({ target: event.target })
     const buttonText = event.target.textContent
 
-    console.log(buttonText) // + 9 8 1 =
+    // console.log(buttonText) // + 9 8 1 =
 
     // TODO: Añadiremos la lógica de la calculadora
+
+    if ('+-*'.includes(buttonText)) {
+      console.log('presionaste el operador', buttonText)
+    } else if (buttonText === '=') {
+      console.log('presionaste el signo', buttonText)
+    } else if (buttonText === 'AC') {
+      console.log('presionaste el boton', buttonText)
+    } else {
+      // Cuando hemos presionado solo los números
+      console.log('presionaste el número', buttonText)
+      numeroActual = numeroActual + buttonText
+    }
+
+    inputDisplay.value = numeroActual
   })
 })
 
