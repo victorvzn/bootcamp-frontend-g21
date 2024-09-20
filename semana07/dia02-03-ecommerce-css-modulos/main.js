7bff04e0ccab211198b12393defdbbeeebe429d6
Victor Villazón <victorzn23@gmail.com>
2024-09-19 22:39:07 -0500

    semana07, dia02, varios

--- semana07/dia02-03-ecommerce-css-modulos/main.js
+++ semana07/dia02-03-ecommerce-css-modulos/main.js
@@ -1,1 +1,29 @@
 // TODO: Al hacer click en los botones - y + decremente o incremente la cantidad.
+
+const menos = document.querySelector('#menos')
+const mas = document.querySelector('#mas')
+const cantidad = document.querySelector('#cantidad')
+
+let contador = 0
+
+mas.addEventListener('click', function(event) {
+  contador = contador + 1
+
+  if (contador > 10) {
+    contador = 10
+    return
+  }
+
+  cantidad.textContent = contador
+})
+
+menos.addEventListener('click', function(event) {
+  contador = contador - 1
+
+  if (contador < 0) {
+    contador = 0
+    return
+  }
+
+  cantidad.textContent = contador
+})
