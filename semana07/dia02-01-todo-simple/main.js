957dc9d4de45228f1eda346d6fbc836e683fcd51
Victor Villazón <victorzn23@gmail.com>
2024-09-19 20:58:46 -0500

    semana07, dia02, TODO simple

--- semana07/dia02-01-todo-simple/main.js
+++ semana07/dia02-01-todo-simple/main.js
@@ -5,11 +5,28 @@ const taskList = document.querySelector('.task__list')
 taskAdd.addEventListener('click', function(event) {
   // Se ejecutará cuando hagamos click en el botón "Añadir tarea"
 
+  if (taskInput.value.trim() === '') {
+    alert('El campo es requerido, ingresa una tarea.')
+    return
+  }
+
   const li = document.createElement('li')
 
-  li.textContent = taskInput.value
+  let checkbox = document.createElement('input')
+  checkbox.setAttribute('type', 'checkbox')
+  li.appendChild(checkbox)
+
+  let span = document.createElement('span')
+  span.textContent = taskInput.value
+  li.appendChild(span)
+  // li.textContent = taskInput.value
+
+  // TODO: Añadir un botón al li con el texto 'Borrar'
+  
 
   taskList.appendChild(li)
+
+  taskInput.value = ''
 
   // const button = document.createElement('button')
   // button.textContent = '¡Hola soy un botón!'
