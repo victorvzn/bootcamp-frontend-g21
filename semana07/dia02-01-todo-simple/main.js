9945e121899ee16d6d9ac5cb4b76b32d343a3b20
Victor Villazón <victorzn23@gmail.com>
2024-09-19 20:37:21 -0500

    semana07, dia02, TODO simple

--- semana07/dia02-01-todo-simple/main.js
+++ semana07/dia02-01-todo-simple/main.js
@@ -5,13 +5,14 @@ const taskList = document.querySelector('.task__list')
 taskAdd.addEventListener('click', function(event) {
   // Se ejecutará cuando hagamos click en el botón "Añadir tarea"
 
-  const button = document.createElement('button')
-  button.textContent = '¡Hola soy un botón!'
-  document.body.appendChild(button)
+  const li = document.createElement('li')
 
+  li.textContent = taskInput.value
+
+  taskList.appendChild(li)
+
+  // const button = document.createElement('button')
+  // button.textContent = '¡Hola soy un botón!'
+  // document.body.appendChild(button)
   // TODO: Añadir dinámicamente una etiqueta h1 dentro del body
-
-  
-
-  console.log('click')
 })
