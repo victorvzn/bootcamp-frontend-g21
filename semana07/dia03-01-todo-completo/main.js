cb595be1ce1347700b7b75d1699c17dd50a38fd3
Victor Villazón <victorzn23@gmail.com>
2024-09-20 20:31:16 -0500

    semana07, dia03, javascript todo completo

--- semana07/dia03-01-todo-completo/main.js
+++ semana07/dia03-01-todo-completo/main.js
@@ -2,6 +2,21 @@ const taskInput = document.querySelector('.task__input')
 const taskClear = document.querySelector('.task__clear')
 const taskList = document.querySelector('.task__list')
 
+let tasks = [
+  {
+    title: 'Estudiar Javascript',
+    completed: true
+  },
+  {
+    title: 'Salir al receso a las 9:00pm',
+    completed: false
+  },
+  {
+    title: 'Realizar el reto del fin de semana',
+    completed: false
+  }
+]
+
 taskInput.addEventListener('keydown', function(event) {
   // console.log(event.key)
 
@@ -9,6 +24,13 @@ taskInput.addEventListener('keydown', function(event) {
 
   if (event.key === 'Enter') {
     console.log('Agregando una nueva tarea', value)
+
+    tasks.push({
+      title: value,
+      completed: false
+    })
+
+    console.log(tasks)
 
     // event.target.value = ''
     taskInput.value = ''
