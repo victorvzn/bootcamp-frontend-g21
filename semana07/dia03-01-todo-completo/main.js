ebbfe53134581abe0af43608484e6a35fedd916a
Victor Villazón <victorzn23@gmail.com>
2024-09-20 20:18:17 -0500

    semana07, dia03, javascript todo completo

--- semana07/dia03-01-todo-completo/main.js
+++ semana07/dia03-01-todo-completo/main.js
@@ -1,1 +1,16 @@
-console.log('Hola JS')
+const taskInput = document.querySelector('.task__input')
+const taskClear = document.querySelector('.task__clear')
+const taskList = document.querySelector('.task__list')
+
+taskInput.addEventListener('keydown', function(event) {
+  // console.log(event.key)
+
+  const value = event.target.value
+
+  if (event.key === 'Enter') {
+    console.log('Agregando una nueva tarea', value)
+
+    // event.target.value = ''
+    taskInput.value = ''
+  }
+})
