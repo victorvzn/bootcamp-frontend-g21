9203c4092af751c3ab0ad999b0888f4e4ddc2828
Victor Villazón <victorzn23@gmail.com>
2024-09-25 21:35:01 -0500

    semana08, dia01, 02, javascript hangman

--- semana08/dia01-02-javascript-hangman/main.js
+++ semana08/dia01-02-javascript-hangman/main.js
@@ -10,14 +10,30 @@ const ALPHABET = Array.from({ length: 26 }, (_, index) => String.fromCharCode(65
 
 let correctGuesses = ''
 
+const selectLetter = (letter) => {
+  console.log('seleccionando letra', letter)
+}
+
+const renderMaskedWord = () => {
+  elMaskedWord.textContent = word
+    .split('')
+    .map(
+      letter => correctGuesses.includes(letter) ? letter : '_'
+    )
+    .join('')
+
+  // SI GANA EL JUEGO HAY QUE AGREGAR MÁS LOGICA
+}
+
 const renderAlphabet = () => {
   let list = ''
 
   ALPHABET.forEach((letter) => {
-    list += `<button onclick="selectLetter">${letter}</button>`
+    list += `<button onclick="selectLetter('${letter}')">${letter}</button>`
   })
 
   elButtons.innerHTML = list
 }
 
+renderMaskedWord()
 renderAlphabet()
