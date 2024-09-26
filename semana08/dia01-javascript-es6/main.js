e834d9ab6a75a1f5b6bcb9f50617c562549fb0c5
Victor Villazón <victorzn23@gmail.com>
2024-09-25 19:43:49 -0500

    semana08, dia01, javascript ES6

--- semana08/dia01-javascript-es6/main.js
+++ semana08/dia01-javascript-es6/main.js
@@ -1,1 +1,64 @@
 // console.log('Hola JS')
+
+// FUNCIONES
+
+// Funciones sin parámetros
+
+function nombreDeLaFuncion() { // Estamos declarando la función
+  console.log('Hola a todos')
+}
+
+nombreDeLaFuncion() // Está ejecutandose la función
+
+// Funciones con parámetros por defecto con retorno y en múltiples líneas
+
+function calculariEsMayorDe18(edad = 1) {
+  if (edad > 18) {
+    return 'Es mayor de edad'
+  } else {
+    return 'Es menor de edad'
+  }
+}
+
+console.log(calculariEsMayorDe18())
+console.log(calculariEsMayorDe18(25))
+
+// ARROW FUNCTIONS (Funciones flecha) =>
+
+// function nombreDeLaFuncion() {
+//   console.log('Hola a todos')
+// }
+
+// Funciones sin parámetros en un línea
+
+const imprimiendoSaludo = () => console.log('Hola a todos nuevamente!')
+
+imprimiendoSaludo()
+
+// Funciones con parámetros en un línea
+
+const suma = (numero1, numero2) => numero1 + numero2
+
+console.log(suma(2, 5))
+
+// Funciones con parámetros y retorno en múltiples líneas
+
+const saludoEnMayusculas = (nombre) => {
+  const nombreEnMayusculas = nombre.toUpperCase()
+
+  return 'Hola ' + nombreEnMayusculas + '!'
+}
+
+console.log(saludoEnMayusculas('Roger'))
+
+// Funciones con parámetros por defecto con retorno y en múltiples líneas
+
+const otroSaludoEnMayusculas = (nombre='Anónimo') => {
+  const nombreEnMayusculas = nombre.toUpperCase()
+
+  return 'Hola ' + nombreEnMayusculas + '!'
+}
+
+console.log(otroSaludoEnMayusculas())
+console.log(otroSaludoEnMayusculas('Roger'))
+
