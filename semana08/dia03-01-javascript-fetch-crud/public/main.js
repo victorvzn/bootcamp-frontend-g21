4370208273aced163ce02bab8c735000c9f5b30f
Victor Villazón <victorzn23@gmail.com>
2024-10-02 20:20:39 -0500

    semana08, dia03, 01, javascript fetch crud

--- semana08/dia03-01-javascript-fetch-crud/public/main.js
+++ semana08/dia03-01-javascript-fetch-crud/public/main.js
@@ -1,5 +1,29 @@
 import { fetchPeliculas } from './services.js'
 import { renderPeliculas } from './utils.js'
 
+const peliculasForm =  document.querySelector('#peliculasForm')
+
+peliculasForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  const peliculaForm = document.forms['peliculasForm']
+
+  const nombre = peliculaForm.nombre.value
+  const imagen = peliculaForm.imagen.value
+  const estreno = peliculaForm.estreno.value
+  const genero = peliculaForm.genero.value
+  const resumen = peliculaForm.resumen.value
+
+  const nuevaPelicula = {
+    nombre,
+    imagen,
+    estreno,
+    generoId: genero,
+    resumen
+  }
+
+  console.log(nuevaPelicula)
+})
+
 fetchPeliculas()
   .then(peliculas => renderPeliculas(peliculas))
