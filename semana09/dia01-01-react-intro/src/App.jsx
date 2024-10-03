157694c48b1a8db6e93cb23baf53eb82fa545520
Victor Villazón <victorzn23@gmail.com>
2024-10-02 22:23:51 -0500

    semana08, dia02, 01, javascript fetch asyn-await

---
+++ semana09/dia01-01-react-intro/src/App.jsx
@@ -0,0 +1,51 @@
+// COMPONENTE USANDO FUNCTION
+
+// function App() {
+//   return (
+//     <h1>Hola React.js</h1>
+//   )
+// }
+
+// export default App
+
+
+// COMPONENTE USANDO ARROW FUNCTIONS
+
+// const App = () => {
+//   return (
+//     <h1>Hola React.js usando Arrow Functions</h1>
+//   )
+// }
+
+// export default App
+
+// COMPONENTES DE REACT USANDO MÚLTIMPLES LÍNEAS CON UN FRAGMENT
+
+// const App = () => {
+//   return (
+//     <>
+//       <h1>Hola React.js</h1>
+//       <p>Estoy en el bootcamp!</p>
+//     </>
+//   )
+// }
+
+// export default App
+
+// EXTENSIONS NECESARIAS PARA USAR REACT EN VSCODE
+
+// ES7+ React/Redux/React-Native snippets
+// https://marketplace.visualstudio.com/items?itemName=dsznajder.es7-react-js-snippets
+
+// SNIPPETS PARA CREAR COMPONENTES
+
+// rafce y rfc
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
+
