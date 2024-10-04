077de2ba3dcd128c4faaafae4e65fdf7c116bdee
Victor Villazón <victorzn23@gmail.com>
2024-10-03 20:00:55 -0500

    semana08, dia01, 02, react intro y imc app

--- semana09/dia01-02-react-imc-app/src/App.jsx
+++ semana09/dia01-02-react-imc-app/src/App.jsx
@@ -10,6 +10,35 @@ const App = () => {
     setWeight(event.target.value)
   }
 
+  const handleHeight = (event) => {
+    setHeight(event.target.value)
+  }
+
+  const imc = weight / ((height / 100) ** 2)
+
+  // * Baja: < 18.5
+  // * Normal: < 18.5 - 24.9
+  // * Sobrepeso: < 25 - 29.9 
+  // * Obeso: > 30
+
+  const imcResult = () => {
+    let result = ''
+
+    if (imc < 18.5) {
+      result = 'Baja'
+    } else if (imc >= 18.5 && imc <= 24.9) {
+      result = 'Normal'
+    } else if (imc >= 25 && imc <= 29.9) {
+      result = 'Sobrepeso'
+    } else if (imc > 30) {
+      result = 'Obeso'
+    } else {
+      result = 'Datos incorrectos'
+    }
+
+    return result
+  }
+
   return (
     <section className="w-[400px] mx-auto bg-slate-200 p-4 mt-8">
       <h1 className="text-3xl text-red-600 font-bold text-center">IMC Calculator</h1>
@@ -21,6 +50,7 @@ const App = () => {
           min="50"
           max="200"
           onChange={handleWeight}
+          value={weight}
         />
       </div>
 
@@ -30,12 +60,14 @@ const App = () => {
           type="range"
           min="50"
           max="200"
+          onChange={handleHeight}
+          value={height}
         />
       </div>
 
-      <p className="font-bold ">Tu IMC es 0</p>
+      <p className="font-bold ">Tu IMC es {imc}</p>
 
-      <p className="font-bold text-2xl">Estado de IMC: 0</p>
+      <p className="font-bold text-2xl">Estado de IMC: {imcResult()}</p>
     </section>
   )
 }
