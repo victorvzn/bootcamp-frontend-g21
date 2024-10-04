6d150552787a898808a9f60b696449ba49e26f3b
Victor Villazón <victorzn23@gmail.com>
2024-10-03 21:41:06 -0500

    semana08, dia02, 03, react todo app

--- semana09/dia02-03-react-todo-app/src/App.jsx
+++ semana09/dia02-03-react-todo-app/src/App.jsx
@@ -49,38 +49,27 @@ const App = () => {
 
       <section className="mt-4">
         <ul className="flex flex-col gap-2">
-          <li className="flex">
-            <input
-              className="mr-2"
-              type="checkbox"
-            />
-            <div className="w-full flex justify-between items-center">
-              <span className="font-medium">
-                title
-              </span>
-              <button
-                className="bg-red-300 rounded-lg px-1 py-1"
-              >
-                ❌
-              </button>
-            </div>
-          </li>
-          <li className="flex">
-            <input
-              className="mr-2"
-              type="checkbox"
-            />
-            <div className="w-full flex justify-between items-center">
-              <span className="font-medium">
-                title
-              </span>
-              <button
-                className="bg-red-300 rounded-lg px-1 py-1"
-              >
-                ❌
-              </button>
-            </div>
-          </li>
+          {todos.map(todo => {
+            return (
+              <li className="flex" key={todo.id}>
+                <input
+                  className="mr-2"
+                  type="checkbox"
+                />
+                <div className="w-full flex justify-between items-center">
+                  <span className="font-medium">
+                    {todo.title}
+                  </span>
+                  <button
+                    className="bg-red-300 rounded-lg px-1 py-1"
+                  >
+                    ❌
+                  </button>
+                </div>
+              </li>
+            )
+          })}
+
         </ul>
       </section>
 
