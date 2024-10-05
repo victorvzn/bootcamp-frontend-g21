ddeb5f3bf48d3b47ac9c22828d490cdcd81add1c
Victor Villazón <victorzn23@gmail.com>
2024-10-04 22:47:33 -0500

    semana08, dia03, 02, react student list reto

---
+++ semana09/dia03-02-react-student-list-reto/src/App.jsx
@@ -0,0 +1,13 @@
+import Avatar from "boring-avatars";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl text-center">Student List</h1>
+
+      <Avatar name="Victir" variant="beam" size={48}/>
+    </>
+  )
+}
+
+export default App
