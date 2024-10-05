f66259b0c8e625716a0d22ac1af37910615389f5
Victor Villazón <victorzn23@gmail.com>
2024-10-04 22:21:08 -0500

    semana08, dia03, 03, react calculadora

--- semana09/dia03-01-react-calculadora/src/App.jsx
+++ semana09/dia03-01-react-calculadora/src/App.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react"
+
 const App = () => {
   // TODO: Hacer la calculadora usando React.js
+  const [numeroActual, setNumeroActual] = useState('0')
+  const [operador, setOperador] = useState('')
+  const [operando, setOperando] = useState('')
+
+  const handleButton = (event) => {
+    const buttonText = event.target.textContent
+    
+    
+  }
 
   return (
     <main className="bg-orange-100 w-80 mx-auto text-center mt-5 p-5">
@@ -10,29 +21,29 @@ const App = () => {
           className="border px-2 py-2 text-right text-xl"
           type="text"
           readOnly
-          value="0"
+          value={numeroActual}
         />
 
         <div className="grid grid-cols-3 gap-2">
-          <button className="bg-red-300 p-2 hover:bg-red-400 duration-300">+</button>
-          <button className="bg-red-300 p-2 hover:bg-red-400 duration-300">-</button>
-          <button className="bg-red-300 p-2 hover:bg-red-400 duration-300">*</button>
+          <button onClick={handleButton} className="bg-red-300 p-2 hover:bg-red-400 duration-300">+</button>
+          <button onClick={handleButton} className="bg-red-300 p-2 hover:bg-red-400 duration-300">-</button>
+          <button onClick={handleButton} className="bg-red-300 p-2 hover:bg-red-400 duration-300">*</button>
 
-          <button className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">7</button>
-          <button className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">8</button>
-          <button className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">9</button>
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">7</button>
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">8</button>
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">9</button>
 
-          <button className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">4</button>
-          <button className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">5</button>
-          <button className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">6</button>
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">4</button>
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">5</button>
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">6</button>
 
-          <button className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">1</button>
-          <button className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">2</button>
-          <button className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">3</button>
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">1</button>
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">2</button>
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">3</button>
 
-          <button className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">0</button>
-          <button className="bg-green-300 p-2 hover:bg-green-400 duration-300">AC</button>
-          <button className="bg-green-300 p-2 hover:bg-green-400 duration-300">=</button>
+          <button onClick={handleButton} className="bg-blue-300 p-2 hover:bg-blue-400 duration-300">0</button>
+          <button onClick={handleButton} className="bg-green-300 p-2 hover:bg-green-400 duration-300">AC</button>
+          <button onClick={handleButton} className="bg-green-300 p-2 hover:bg-green-400 duration-300">=</button>
         </div>
       </section>
     </main>
