52b32e4e9322b678a6d5cfdb4f78b1e575fd3148
Victor Villazón <victorzn23@gmail.com>
2024-10-09 19:49:20 -0500

    semana08, dia03, 02, react student list reto

--- semana09/dia03-02-react-student-list-reto/src/App.jsx
+++ semana09/dia03-02-react-student-list-reto/src/App.jsx
@@ -1,12 +1,44 @@
 import Avatar from "boring-avatars";
+import { useState } from "react";
 
 const App = () => {
+  const DEFAULT_STUDENTS = [
+    {
+      id: "1",
+      name: "Bulma",
+      city: 'Chiclayo'
+    },
+    {
+      id: "2",
+      name: "Goku",
+      city: 'Trujillo'
+    },
+    {
+      id: "3",
+      name: "Vegeta",
+      city: 'Lima'
+    }
+  ]
+
+  const [students, setStudents] = useState(DEFAULT_STUDENTS)
+
   return (
-    <>
+    <main className="">
       <h1 className="text-3xl text-center">Student List</h1>
 
-      <Avatar name="Victir" variant="beam" size={48}/>
-    </>
+      <div className="student__list">
+        <div className="student__row">
+          <div>AVATAR</div>
+          <Avatar name="Victir" variant="beam" size={48}/>
+          <div>NAME</div>
+          <div>CIUDAD</div>
+          <div>
+            <button>✏</button>
+            <button>❌</button>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
