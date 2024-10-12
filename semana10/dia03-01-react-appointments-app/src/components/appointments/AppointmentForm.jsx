cb1d6ccee1cd7435ece5c28eadb417b777e290ef
Victor Villazón <victorzn23@gmail.com>
2024-10-11 20:19:51 -0500

    semana10, dia03, 01, react appointments app

--- semana10/dia03-01-react-appointments-app/src/components/appointments/AppointmentForm.jsx
+++ semana10/dia03-01-react-appointments-app/src/components/appointments/AppointmentForm.jsx
@@ -1,6 +1,53 @@
 const AppointmentForm = () => {
   return (
-    <div>AppointmentForm</div>
+    <section className="w-96 p-4">
+      <h2 className="text-2xl text-center mb-4">Nuevo paciente</h2>
+
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          name="petName"
+          placeholder="Nombre de la mascota"
+          className="border rounded-md p-3 shadow-lg"
+        />
+        <input
+          type="text"
+          name="petAge"
+          placeholder="Edad de la mascota"
+          className="border rounded-md p-3 shadow-lg"
+        />
+        <input
+          type="text"
+          name="ownerName"
+          placeholder="Nombre del dueño"
+          className="border rounded-md p-3 shadow-lg"
+        />
+        <input
+          type="text"
+          name="appointmentDate"
+          placeholder="Fecha de la cita"
+          className="border rounded-md p-3 shadow-lg"
+        />
+        <input
+          type="text"
+          name="appointmentTime"
+          placeholder="Hora de la cita"
+          className="border rounded-md p-3 shadow-lg"
+        />
+        <textarea
+          name="symptoms"
+          placeholder="Síntomas"
+          className="border rounded-md p-3 shadow-lg"
+          rows={3}
+        />
+
+        <input
+          type="submit"
+          className="border p-2 bg-green-800 text-white rounded-md cursor-pointer"
+          value="Guardar"
+        />
+      </form>
+    </section>
   )
 }
 
