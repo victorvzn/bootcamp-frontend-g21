0b2923519ac305481cf837ec6a8aa09d62016f09
Victor Villazón <victorzn23@gmail.com>
2024-10-11 20:41:14 -0500

    semana10, dia03, 01, react appointments app

--- semana10/dia03-01-react-appointments-app/src/components/appointments/AppointmentForm.jsx
+++ semana10/dia03-01-react-appointments-app/src/components/appointments/AppointmentForm.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react"
+
 const AppointmentForm = () => {
+  const INITIAL_FORM_STATE = {
+    id: '',
+    petName: '',
+    petAge: '',
+    ownerName: '',
+    appointmentDate: '',
+    appointmentTime: '',
+    symptoms: ''
+  }
+
+  const [form, setForm] = useState(INITIAL_FORM_STATE)
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+
+    setForm({ ...form, [name]: value })
+    // console.log(name, value)
+  }
+
   return (
     <section className="w-96 p-4">
       <h2 className="text-2xl text-center mb-4">Nuevo paciente</h2>
+
+      <pre>{JSON.stringify(form, null, 2)}</pre>
 
       <form className="flex flex-col gap-4">
         <input
@@ -9,36 +32,50 @@ const AppointmentForm = () => {
           name="petName"
           placeholder="Nombre de la mascota"
           className="border rounded-md p-3 shadow-lg"
+          onChange={handleChange}
+          value={form.petName}
         />
         <input
-          type="text"
+          type="number"
           name="petAge"
+          min="0"
+          max="30"
           placeholder="Edad de la mascota"
           className="border rounded-md p-3 shadow-lg"
+          onChange={handleChange}
+          value={form.petAge}
         />
         <input
           type="text"
           name="ownerName"
           placeholder="Nombre del dueño"
           className="border rounded-md p-3 shadow-lg"
+          onChange={handleChange}
+          value={form.ownerName}
         />
         <input
-          type="text"
+          type="date"
           name="appointmentDate"
           placeholder="Fecha de la cita"
           className="border rounded-md p-3 shadow-lg"
+          onChange={handleChange}
+          value={form.appointmentDate}
         />
         <input
-          type="text"
+          type="time"
           name="appointmentTime"
           placeholder="Hora de la cita"
           className="border rounded-md p-3 shadow-lg"
+          onChange={handleChange}
+          value={form.appointmentTime}
         />
         <textarea
           name="symptoms"
           placeholder="Síntomas"
           className="border rounded-md p-3 shadow-lg"
           rows={3}
+          onChange={handleChange}
+          value={form.symptoms}
         />
 
         <input
