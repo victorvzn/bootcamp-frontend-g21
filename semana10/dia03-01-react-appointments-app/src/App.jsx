6c966a1a15b11b8a097822fe7e2ba6580af40f06
Victor Villazón <victorzn23@gmail.com>
2024-10-11 19:50:01 -0500

    semana10, dia03, 01, react appointments app

--- semana10/dia03-01-react-appointments-app/src/App.jsx
+++ semana10/dia03-01-react-appointments-app/src/App.jsx
@@ -1,12 +1,13 @@
 import { Header } from "./components/Header"
+import Appointments from "./pages/Appointments"
 
 const App = () => {
   return (
     <>
       <Header title='Citas médicas para mascotas' />
 
-      <main>
-        citas
+      <main className="container m-auto flex gap-12 py-5 border">
+        <Appointments />
       </main>
     </>
   )
---
+++ semana10/dia03-01-react-appointments-app/src/pages/Appointments.jsx
@@ -0,0 +1,10 @@
+const Appointments = () => {
+  return (
+    <>
+      <div>AppointmentForm</div>
+      <div>AppointmentList</div>
+    </>
+  )
+}
+
+export default Appointments
