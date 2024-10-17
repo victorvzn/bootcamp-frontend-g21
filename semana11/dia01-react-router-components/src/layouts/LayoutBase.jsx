2a8a380be5a70b55f81b607b72aa1c673f6e0031
Victor Villazón <victorzn23@gmail.com>
2024-10-16 20:45:48 -0500

    semana11, dia01, 01, react router components

--- semana11/dia01-react-router-components/src/layouts/LayoutBase.jsx
+++ semana11/dia01-react-router-components/src/layouts/LayoutBase.jsx
@@ -3,13 +3,18 @@ import { Link, Outlet } from "react-router-dom"
 const LayoutBase = () => {
   return (
     <>
-      <header className='flex gap-2 bg-amber-200 py-4 px-6'>
-        <h1 className='font-bold'>React Router DOM</h1>
-        <Link to='/'>Home</Link>
-        <Link to='/login'>Login</Link>
-        <Link to='/register'>Register</Link>
+      <header className='bg-amber-200 py-4 px-6'>
+        <div className="container mx-auto flex justify-between">
+          <h1 className='font-bold'>React Router DOM</h1>
+
+          <nav className="flex gap-4">
+            <Link to='/'>Home</Link>
+            <Link to='/login'>Login</Link>
+            <Link to='/register'>Register</Link>
+          </nav>
+        </div>
       </header>
-      <main className='container mx-auto border'>
+      <main className='container mx-auto my-6 px-6'>
         <Outlet />
       </main>
     </>
