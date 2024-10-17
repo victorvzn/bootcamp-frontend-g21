407e4f44d0bca97b008594c7a0242f7e0e2dcb9f
Victor Villazón <victorzn23@gmail.com>
2024-10-16 20:07:18 -0500

    semana11, dia01, 01, react router components

--- semana11/dia01-react-router-components/src/App.jsx
+++ semana11/dia01-react-router-components/src/App.jsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import { LoginPage } from './pages/LoginPage'
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/login' element={<h1>LOGIN</h1>} />
+        <Route path='/register' element={<h1>REGISTER</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
