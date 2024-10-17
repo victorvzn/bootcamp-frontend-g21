3f27fff88900e7ee1ec95d7ea3e5ba8104acd0ff
Victor Villazón <victorzn23@gmail.com>
2024-10-16 22:34:38 -0500

    semana11, dia01, 01, react router components

--- semana11/dia01-react-router-components/src/pages/CharacterPage.jsx
+++ semana11/dia01-react-router-components/src/pages/CharacterPage.jsx
@@ -1,6 +1,40 @@
+import { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+
+const getCharacter = async (id) => {
+  const URL = `https://dragonball-api.com/api/characters/${id}`
+
+  const response = await fetch(URL)
+
+  return await response.json()
+}
+
 const CharacterPage = () => {
+  const { id } = useParams()
+
+  const [character, setCharacter] = useState()
+
+  useEffect(() => {
+    getCharacter(id)
+      .then(result => setCharacter(result))
+  }, [])
+
   return (
-    <div>CharacterPaasdasdasdasdasdge</div>
+    <>
+      <h1 className='font-bold text-3xl text-center'>
+        Character detail #{id}
+      </h1>
+
+      <section className='flex justify-center flex-col items-center mt-6 text-center gap-4'>
+        <Link to='/' className='font-bold text-blue-700 hover:underline'>Back home</Link>
+        <img src={character?.image} width={200} />
+        <h2 className='text-4xl mb-2'>{character?.race}</h2>
+        <strong>{character?.originPlanet.name}</strong>
+        <p>{character?.description}</p>
+
+        {/* <pre>{JSON.stringify(character, null, 2)}</pre> */}
+      </section>
+    </>
   )
 }
 
