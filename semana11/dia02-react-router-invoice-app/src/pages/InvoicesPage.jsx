89582ee295d7dc9adbd307eab8be1a63be83b1ff
Victor Villazón <victorzn23@gmail.com>
2024-10-17 22:43:11 -0500

    semana11, dia02, 02, react router invoice app

--- semana11/dia02-react-router-invoice-app/src/pages/InvoicesPage.jsx
+++ semana11/dia02-react-router-invoice-app/src/pages/InvoicesPage.jsx
@@ -1,9 +1,26 @@
 // TODO: Consultar los datos de mockapi y listar los invoices en el navegador
 // https://67074c39a0e04071d229b837.mockapi.io/api/v1/invoices
 
+import { useEffect, useState } from "react"
+
+const fetchInvoices = async () => {
+  const url = 'https://67074c39a0e04071d229b837.mockapi.io/api/v1/invoices'
+
+  const response = await fetch(url)
+
+  return await response.json()
+}
+
 const InvoicesPage = () => {
+  const [invoices, setInvoices] = useState([])
+
+  useEffect(() => {
+    fetchInvoices()
+      .then(invoices => setInvoices(invoices))
+  }, [])
+
   return (
-    <div>InvoicesPage</div>
+    <div>{JSON.stringify(invoices)}</div>
   )
 }
 
