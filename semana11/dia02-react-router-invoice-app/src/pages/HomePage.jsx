d330938e995254cde512afb26a64e9f5c733b04e
Victor Villazón <victorzn23@gmail.com>
2024-10-17 20:15:55 -0500

    semana11, dia02, 02, react router invoice app

--- semana11/dia02-react-router-invoice-app/src/pages/HomePage.jsx
+++ semana11/dia02-react-router-invoice-app/src/pages/HomePage.jsx
@@ -1,6 +1,32 @@
+// TODO: listar los productos del siguiente endpoint renderizandolos en esta página
+// https://dummyjson.com/products
+
+import { useEffect, useState } from "react"
+
+const fetchProducts = async () => {
+  const url = 'https://dummyjson.com/products'
+
+  const response = await fetch(url)
+
+  return await response.json()
+}
+
 const HomePage = () => {
+  const [products, setProduts] = useState([])
+
+  useEffect(() => {
+    fetchProducts()
+      .then(data => setProduts(data.products))
+  }, [])
+
   return (
-    <div>HomePage</div>
+    <section>
+      <h1 className="font-bold">Listado de productos</h1>
+
+      {products.map(product => {
+        return <h3 key={product.id}>{product.title}</h3>
+      })}
+    </section>
   )
 }
 
