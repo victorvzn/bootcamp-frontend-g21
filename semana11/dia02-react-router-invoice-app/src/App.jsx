3a9ae57476ad667856d598ea94a1c5ff17c57131
Victor Villazón <victorzn23@gmail.com>
2024-10-17 22:22:35 -0500

    semana11, dia02, 02, react router invoice app

--- semana11/dia02-react-router-invoice-app/src/App.jsx
+++ semana11/dia02-react-router-invoice-app/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import LoginPage from './pages/LoginPage'
 import HomePage from './pages/HomePage'
+import InvoicesPage from './pages/InvoicesPage'
 
 const App = () => {
   return (
@@ -9,6 +10,9 @@ const App = () => {
       <Routes>
         <Route path='/' element={<LoginPage />} />
         <Route path='/home' element={<HomePage />} />
+
+        <Route path='/invoices' element={<InvoicesPage />} />
+
       </Routes>
     </BrowserRouter>
   )
---
+++ semana11/dia02-react-router-invoice-app/src/pages/InvoicesPage.jsx
@@ -0,0 +1,10 @@
+// TODO: Consultar los datos de mockapi y listar los invoices en el navegador
+// https://67074c39a0e04071d229b837.mockapi.io/api/v1/invoices
+
+const InvoicesPage = () => {
+  return (
+    <div>InvoicesPage</div>
+  )
+}
+
+export default InvoicesPage
