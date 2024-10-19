4053e4345c04c50d632c21bcc9df817694f0fed7
Victor Villazón <victorzn23@gmail.com>
2024-10-18 20:55:43 -0500

    semana11, dia03, 03, react router peliculas crud app

--- semana11/dia03-react-peliculas-crud-fetch-router/src/pages/HomePage.jsx
+++ semana11/dia03-react-peliculas-crud-fetch-router/src/pages/HomePage.jsx
@@ -1,6 +1,45 @@
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Imagen</th>
+          <th>Nombre</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>
+            <img
+              src='https://placehold.co/100x250'
+              width="100"
+              height="250"
+            />
+          </td>
+          <td>
+            <strong>nombre</strong>
+            <div className="fs-small">
+              <strong>Estreno:</strong> 18/10/24
+            </div>
+            <div className="fs-small">
+              <strong>Genero:</strong> Acción
+            </div>
+            <div className="fs-small">
+              <strong>Resumen:</strong> resumen....
+            </div>
+          </td>
+          <td>
+            <div className="flex gap-0.5">
+              <button>Editar</button>
+              <button>Eliminar</button>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   )
 }
 
