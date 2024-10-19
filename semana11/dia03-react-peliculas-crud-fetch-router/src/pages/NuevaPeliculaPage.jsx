d3d61ecad1e2c6de5bb6323d18ea190521ce3f66
Victor Villazón <victorzn23@gmail.com>
2024-10-18 22:49:47 -0500

    semana11, dia03, 03, react router peliculas crud app

--- semana11/dia03-react-peliculas-crud-fetch-router/src/pages/NuevaPeliculaPage.jsx
+++ semana11/dia03-react-peliculas-crud-fetch-router/src/pages/NuevaPeliculaPage.jsx
@@ -1,55 +1,92 @@
+import { useState } from "react"
+
 const NuevaPeliculaPage = () => {
-  // TODO: 01 - Manejar el estado del formulario.
+  // DONE: 01 - Manejar el estado del formulario.
   // TODO: 02 - Manejar el envio del formulario al endpoint peliculas mediante un POST para guardar en el endpoint.
   // TODO: 03 - Redirigir a la ruta del listado de peliculas después de guardar una película.
+  const [form, setForm] = useState({
+    nombre: '',
+    imagen: '',
+    estreno: '',
+    generoId: '',
+    resumen: ''
+  })
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSave = (event) => {
+    event.preventDefault();
+    
+    console.log('guardando datos...')
+  }
 
   return (
-    <form>
-      <h2>Nueva película</h2>
+    <>
+      <form onSubmit={handleSave}>
+        <h2>Nueva película</h2>
 
-      <label>
-        Nombre
-        <input
-          type="text"
-          name="nombre"
-          placeholder="Ej. Volver al futuro"
-          required
-        />
-      </label>
-      <label>
-        Imagen
-        <input
-          type="url"
-          name="imagen"
-          placeholder="Ej. https://placehold.co/100x250"
-          required
-        />
-      </label>
-      <label>
-        Estreno
-        <input
-          type="date"
-          name="estreno"
-          placeholder="Ej. 2024-10-18"
-          required
-        />
-      </label>
-      <label>
-        Genero
-        <select name="generoId" required>
-          <option value=''>Selecciona un genero</option>
-          <option value='1'>Comedia</option>
-          <option value='2'>Acción</option>
-          <option value='3'>Animación</option>
-          <option value='4'>Aventura</option>
-        </select>
-      </label>
-      <label>
-        Resumen
-        <textarea name="resumen" placeholder="Ej. mi resumen" required></textarea>
-      </label>
-      <input type="submit" value="Guardar" />
-    </form>
+        <label>
+          Nombre
+          <input
+            type="text"
+            name="nombre"
+            placeholder="Ej. Volver al futuro"
+            required
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Imagen
+          <input
+            type="url"
+            name="imagen"
+            placeholder="Ej. https://placehold.co/100x250"
+            required
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Estreno
+          <input
+            type="date"
+            name="estreno"
+            placeholder="Ej. 2024-10-18"
+            required
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Genero
+          <select
+            name="generoId"
+            required
+            onChange={handleChange}
+          >
+            <option value=''>Selecciona un genero</option>
+            <option value='1'>Comedia</option>
+            <option value='2'>Acción</option>
+            <option value='3'>Animación</option>
+            <option value='4'>Aventura</option>
+          </select>
+        </label>
+        <label>
+          Resumen
+          <textarea
+            name="resumen"
+            placeholder="Ej. mi resumen"
+            required
+            onChange={handleChange}
+          ></textarea>
+        </label>
+        <input type="submit" value="Guardar" />
+      </form>
+    
+      <pre>{JSON.stringify(form, null, 2)}</pre>
+    </>
   )
 }
 
