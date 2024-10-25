1289c7f9d2c92e48fd6e2871a3d68739ccb71e1b
Victor Villazón <victorzn23@gmail.com>
2024-10-24 21:53:32 -0500

    semana12, dia02, react zustand counter shopping cart app

--- semana12/dia02-react-zustand-counter-shopping-cart-app/src/components/Counter.jsx
+++ semana12/dia02-react-zustand-counter-shopping-cart-app/src/components/Counter.jsx
@@ -7,14 +7,23 @@ const Counter = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: '#CD8D7A', padding: '1rem' }}>
 
-      <h3>Counter with Zustand</h3>
+      <h3 className="text-3xl mb-4 font-bold">Counter with Zustand</h3>
 
       <div style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
-        <button onClick={() => increaseByValue(15)}>+15</button>
-        <button onClick={increment}>+1</button>
-        <h1>{count}</h1>
-        <button onClick={decrement}>-1</button>
-        <button onClick={() => increaseByValue(-15)}>-15</button>
+        <button
+          className="px-4 py-2 bg-sky-500 rounded-lg font-bold"
+          onClick={() => increaseByValue(15)}
+        >+15</button>
+        <button
+          className="px-4 py-2 bg-sky-500 rounded-lg font-bold"
+          onClick={increment}>+1</button>
+        <h1 className="text-3xl font-bold">{count}</h1>
+        <button
+          className="px-4 py-2 bg-sky-500 rounded-lg font-bold"
+          onClick={decrement}>-1</button>
+        <button
+          className="px-4 py-2 bg-sky-500 rounded-lg font-bold"
+          onClick={() => increaseByValue(-15)}>-15</button>
       </div>
 
     </div>
