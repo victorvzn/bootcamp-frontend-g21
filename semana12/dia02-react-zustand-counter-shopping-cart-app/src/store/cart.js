7b15f63c1deaf7872d8e783274851dee86ac923a
Victor Villazón <victorzn23@gmail.com>
2024-10-24 22:46:16 -0500

    semana12, dia02, react zustand counter shopping cart app

---
+++ semana12/dia02-react-zustand-counter-shopping-cart-app/src/store/cart.js
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+
+export const useCartStore = create(
+  (set, get) => ({
+    // Estado INICIAL
+    cart: [],
+
+    // Actions
+    addToCart: (newProduct) => {
+      
+    },
+    removeFromCart: (id) => {
+
+    },
+    cleanCart: () => {
+
+    }
+  })
+)
