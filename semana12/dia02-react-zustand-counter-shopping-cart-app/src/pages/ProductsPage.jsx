60f8af0a12e4dce2204457606be1d6184bb7d625
Victor Villazón <victorzn23@gmail.com>
2024-10-24 22:21:52 -0500

    semana12, dia02, react zustand counter shopping cart app

--- semana12/dia02-react-zustand-counter-shopping-cart-app/src/pages/ProductsPage.jsx
+++ semana12/dia02-react-zustand-counter-shopping-cart-app/src/pages/ProductsPage.jsx
@@ -1,11 +1,14 @@
 import Header from "../components/Header"
+import ProductList from "../components/ProductList"
 
 const ProductsPage = () => {
   return (
     <main className="mb-10">
       <Header />
 
-      
+      <section className="container py-4 px-6 mx-auto border mt-5">
+        <ProductList />
+      </section>
     </main>
   )
 }
