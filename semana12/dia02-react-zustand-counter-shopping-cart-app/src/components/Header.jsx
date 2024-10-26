7a03a61d53256ab0b78e396b32f239e737bfa98f
Victor Villazón <victorzn23@gmail.com>
2024-10-25 19:24:13 -0500

    semana12, dia02, react zustand counter shopping cart app

--- semana12/dia02-react-zustand-counter-shopping-cart-app/src/components/Header.jsx
+++ semana12/dia02-react-zustand-counter-shopping-cart-app/src/components/Header.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react"
 import { useCartStore } from "../store/cart"
 
 const Header = () => {
   const { cart } = useCartStore()
+
+  const [open, setOpen] = useState(false)
 
   return (
     <header className="bg-lime-600 text-white py-4 px-6 w-full">
@@ -10,12 +13,20 @@ const Header = () => {
 
         <button
           className="text-3xl relative"
+          onClick={() => setOpen(!open)}
         >
           🛒
           <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -end-2 dark:border-gray-900">
             {cart.length}
           </div>
         </button>
+
+        { open && (
+          <div>
+            CARRITO DE COMPRAS
+            {/* {JSON.stringify(cart)}2 */}
+          </div>
+        )}
       </div>
     </header>
   )
